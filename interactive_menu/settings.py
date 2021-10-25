2c0906382a46985bcee041c90653267e3efd7b83
Mateus Rodrigues <68032415+mateus-rodriguess@users.noreply.github.com>
2021-10-25 11:01:11 -0300

    ...

--- interactive_menu/settings.py
+++ interactive_menu/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': '1234',
+        'PASSWORD': 'postgres',
         'HOST': 'db',
         'PORT': '5432',
     }
