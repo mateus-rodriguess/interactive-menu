8a3d3c39104e335a4a2401a3e1eb3529cd1a9b4b
Mateus Rodrigues <mateus.rodrigues.sistema@gmail.com>
2021-11-05 17:55:54 -0300

    preparação pra o app estoque

--- apps/inventory/admin.py
+++ apps/inventory/admin.py
@@ -1,12 +1,1 @@
 from django.contrib import admin
-from .models import EstoqueItems, Inventory
-
-
-@admin.register(Inventory)
-class InventoryAdmin(admin.ModelAdmin):
-    pass
-    
-
-@admin.register(EstoqueItems)
-class EstoqueItemsAdmin(admin.ModelAdmin):
-    pass
