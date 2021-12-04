973bd2e217ab9b3039c0e04ae280c2d4d957187d
Mateus Rodrigues <mateus.rodrigues.sistema@gmail.com>
2021-12-03 21:43:20 -0300

    .....

---
+++ apps/menu/tests/conftest.py
@@ -0,0 +1,7 @@
+import pytest
+
+
+@pytest.fixture
+def Table_data():
+    return {'number': 2, 'description': 'descrição', 
+            'max': 2}
--- apps/menu/tests/tests_menu.py
+++ apps/menu/tests/tests_menu.py
@@ -1,11 +1,40 @@
 from django import urls
 import pytest
+from mixer.backend.django import mixer
+from apps.menu.models import Category, Table
 
 
 @pytest.mark.django_db
 @pytest.mark.parametrize('param',[
     ('menu:product_list')])
-def test_manu_list_view(client, param):
-	temp_url = urls.reverse(param)
-	resp = client.get(temp_url)
-	assert resp.status_code == 200
+class TestMenu:
+	
+	def test_manu_list_view(self, client, param):
+		temp_url = urls.reverse(param)
+		resp = client.get(temp_url)
+		assert resp.status_code == 200
+
+
+@pytest.mark.django_db
+class TestModel:
+	
+	def test_create_category(self):
+		category = Category
+		assert category.objects.count() == 0
+		category.objects.create(name="categoria")
+		assert category.objects.count() == 1
+
+	def test_model_display_category(self):
+		category = mixer.blend(Category, name="categoria")
+		assert str(category) ==  "categoria"
+
+	def test_create_table(self):
+		table =  Table
+		assert table.objects.count() == 0
+		Table.objects.create(number=2, description="descrição", max=4)
+		assert table.objects.count() == 1
+
+	def test_model_display_table(self):
+		table = mixer.blend(Table, number=5)
+		assert str(table) ==  "5"
+
